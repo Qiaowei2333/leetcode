7a6e521bf70a00c4b59e8c18ab7c68a05c12bc2a
Qiaowei Li <qiaoweili@Qiaoweis-MBP.attlocal.net>
2019-07-09 23:27:20 -0500

    lt25 solution1 not very efficient

--- algorithms/LinkedList/Solution25.java
+++ algorithms/LinkedList/Solution25.java
@@ -2,28 +2,24 @@ import java.util.Stack;
 
 class Solution25 {
     public static ListNode reverseKGroup(ListNode head, int k) {
-         if(head == null) return null;
-         ListNode dummy = new ListNode(-1);
-         dummy.next = head;
-         ListNode cur = dummy;
-         ListNode next = null;
-         Stack<ListNode> st = new Stack<ListNode>();
-         int i = k;
-         while(head != null) {
-             st.push(head);
-             head = head.next;
-             i--;
-             if(i == 0) {
-                 next = head;
-                 while(!st.isEmpty()) {
-                     cur.next = st.pop();
-                     cur = cur.next;
-                 }
-             }
-         }
-         if (i!=0) {
-             cur.next = next;
-         }
-         return dummy.next;
-     }
+        if(head == null) return null;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = dummy;
+        ListNode next = dummy.next;;
+        Stack<ListNode> st = new Stack<ListNode>();
+        while(next != null) {
+            for(int i = 0; i < k && next != null; i++) {
+                st.push(next);
+                next = next.next;
+            }
+            if(st.size() < k) return dummy.next;
+            while(st.size() != 0) {
+                cur.next = st.pop();
+                cur = cur.next;
+            }
+            cur.next = next;
+        }
+        return dummy.next;
+    }
  }
