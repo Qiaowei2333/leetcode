0c614a6f0dbc338661e853e5b99fd89b44a1cae6
Qiaowei Li <qiaoweili@Qiaoweis-MacBook-Pro.local>
2019-08-23 22:14:07 -0500

    lint74 find first bad version

---
+++ algorithms/BinarySearch/FirstBadVersion.java
@@ -0,0 +1,31 @@
+/**
+ * public class SVNRepo {
+ *     public static boolean isBadVersion(int k);
+ * }
+ * you can use SVNRepo.isBadVersion(k) to judge whether 
+ * the kth code version is bad or not.
+*/
+// Find first bad = true
+class FirstBadVersion {
+    public int findFirstBadVersion(int n) {
+        int start = 1, end = n;
+        while(start + 1 < end) {
+            int mid = start + (end - start)/2;
+            if(SVNRepo.isBadVersion(mid)) {
+                end = mid;
+            }
+            else {
+                start = mid;
+            }
+        } 
+        if(SVNRepo.isBadVersion(start)) {
+            return start;
+        }
+        else if(SVNRepo.isBadVersion(end)) {
+            return end;
+        }
+        else {
+            return 0;
+        }
+    }
+}
