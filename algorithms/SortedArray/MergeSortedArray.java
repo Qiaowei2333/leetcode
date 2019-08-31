90a6edb942db60ce1a50ed373613e353560bd769
Qiaowei Li <qiaoweili@Qiaoweis-MacBook-Pro.local>
2019-08-30 22:36:52 -0500

    lint6 merge 2 Sorted array into new array

---
+++ algorithms/SortedArray/MergeSortedArray.java
@@ -0,0 +1,34 @@
+// lint64
+
+public class MergeSortedArray {
+    public int[] mergeSortedArray(int[] A, int[] B) {
+        if(A == null || B == null) {
+            return null;
+        }
+
+        int index = 0, i = 0, j = 0;
+        int[] result = new int[A.length + B.length];
+        
+        while(i < A.length && j < B.length) {
+            if(A[i] < B[j]) {
+                result[index++] = A[i++];
+            }
+            else {
+                result[index++] = B[j++];
+            }
+        }
+
+        while(i < A.length) {
+            result[index++] = A[i++];
+        }
+        while(j < B.length) {
+            result[index++] = B[j++];
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(1);
+
+    }
+}
