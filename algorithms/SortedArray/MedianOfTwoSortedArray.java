c2513c3d51cae23784728958d029726a21fb1f22
Qiaowei Li <qiaoweili@Qiaoweis-MacBook-Pro.local>
2019-09-09 21:38:06 -0500

    lint65 median of 2 sorted array - merge two array and find median

---
+++ algorithms/SortedArray/MedianOfTwoSortedArray.java
@@ -0,0 +1,38 @@
+// lin65 sort two arrays first
+public class MedianOfTwoSortedArray {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int[] newArray = new int[A.length + B.length];
+        int i = 0, j = 0, m = 0;
+        while(i < A.length && j < B.length) {
+            if(A[i] > B[j]) {
+                newArray[m++] = B[j++];
+            }
+            else {
+                newArray[m++] = A[i++];
+            }
+        }
+
+        while(i < A.length) {
+            newArray[m++] = A[i++];
+        }
+        while(j < B.length) {
+            newArray[m++] = B[j++];
+        }   
+        
+        if(m % 2 == 0) {
+            return ((double)(newArray[m / 2]) + (double)newArray[m / 2 - 1]) / 2;
+        }
+        else {
+            return (double)newArray[m / 2];
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        MedianOfTwoSortedArray s = new MedianOfTwoSortedArray();
+        int[] A = {1, 2, 3, 4, 5, 6};
+        int[] B = {2, 3, 4, 5};
+        double result = s.findMedianSortedArrays(A, B);
+        System.out.println(result);
+    }
+}
