c26c9a1b1ab1c2f82c9195cf9a9fa2ad67c01a57
Qiaowei2333 <qxl161930@utdallas.edu>
2019-10-10 00:25:23 -0500

    Lint53 reverse words in a string, very good test for split()

---
+++ algorithms/SortedArray/ReverWordsInAString.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class ReverWordsInAString {
+    public String reverseWords(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        String[] words = s.split(" ");
+        if (words.length == 0) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        int i = words.length - 1;
+        for (; i >= 0; i--) {
+            if (words[i].equals("")) {
+                continue;
+            }
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        System.out.println(sb.length());
+        return sb.substring(0, sb.length()-1).toString();
+    }
+
+
+    public static void main(String[] args) {
+        ReverWordsInAString s = new ReverWordsInAString();
+        String ss = "1111  11 1";
+        String[] ss1 = ss.split("1");
+        System.out.println(ss1.length);
+        for (String ss2 : ss1) {
+            System.out.println("'"+ ss2 +"'");
+        }
+
+        String result = s.reverseWords(" ");
+        System.out.println("'" + result + "'");
+    }
+}
