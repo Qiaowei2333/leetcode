0eda4bd8a71c8b75469a86dbd0a9e1d7a76e7709
Qiaowei2333 <qxl161930@utdallas.edu>
2019-10-23 01:42:58 -0500

    Lint97 maximum depth of binary tree - divide and conquer

---
+++ algorithms/BinaryTree_Divide_Conquer_BFS_DSF/MaximumDepthOfBinaryTreeDivConq.java
@@ -0,0 +1,34 @@
+public class MaximumDepthOfBinaryTreeDivConq{
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(17);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+        MaximumDepthOfBinaryTreeDivConq s = new MaximumDepthOfBinaryTreeDivConq();
+        int result = s.maxDepth(node1);
+        System.out.println(result);
+    }
+}
