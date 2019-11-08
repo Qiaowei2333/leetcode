5d7bf987100842990dfccfc0df216c4653ad6dcc
Qiaowei2333 <qxl161930@utdallas.edu>
2019-11-07 21:37:18 -0600

    Lint94 binaryTreeMaxPathSum - divide and conquer huahuajiang

---
+++ algorithms/BinaryTree_Divide_Conquer_BFS_DSF/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,46 @@
+// https://www.youtube.com/watch?v=9ZNky1wqNUw 花花酱解释
+public class BinaryTreeMaximumPathSum {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+    private int maxValue;
+
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;   
+    }
+
+    // path with max sum, must include current node 
+    private int maxPathDown(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, maxPathDown(root.left));
+        int right = Math.max(0, maxPathDown(root.right));
+
+        maxValue = Math.max(maxValue, left + right + root.val); // 把这一句去掉， 就是find a path including current node with max sum
+                                                                // 这就是 maxPathDown 的含义
+        return Math.max(left, right) + root.val;
+    }
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(17);
+        node1.left = node2;
+        node1.right = node3;
+    
+        BinaryTreeMaximumPathSum s = new BinaryTreeMaximumPathSum();
+        int sum = s.maxPathSum(node1);
+
+        System.out.println(sum);
+    }
+}
