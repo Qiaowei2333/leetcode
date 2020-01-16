25d06bc014293787927c6d3ce0ab8fa145676995
Qiaowei2333 <qxl161930@utdallas.edu>
2020-01-15 19:34:44 -0600

    lint95 div conq

---
+++ algorithms/BinarySearchTree/ValidateBinarySearchTreeDivConq.java
@@ -0,0 +1,20 @@
+// lint 95 div and conq
+import java.util.*;
+public class ValidateBinarySearchTreeTraverse {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
