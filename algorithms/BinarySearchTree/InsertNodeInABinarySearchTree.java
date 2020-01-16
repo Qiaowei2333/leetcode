6ae2e3f574c10b9514c24a5f4207aed5f6e4ccff
Qiaowei2333 <qxl161930@utdallas.edu>
2020-01-16 01:33:54 -0600

    lint85 blank

---
+++ algorithms/BinarySearchTree/InsertNodeInABinarySearchTree.java
@@ -0,0 +1,19 @@
+// lint 85
+public class InsertNodeInABinarySearchTree{
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode insertNode(TreeNode root, TreeNode node) {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
