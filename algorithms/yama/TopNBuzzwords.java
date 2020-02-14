b214f9d0210e905c79b1b5934486b6b23ba78ccb
Qiaowei2333 <qxl161930@utdallas.edu>
2020-02-13 21:58:47 -0600

    top n words - yama

--- algorithms/yama/TopNBuzzwords.java
+++ algorithms/yama/TopNBuzzwords.java
@@ -30,9 +30,12 @@ public class TopNBuzzwords {
         }
 
         for (String quote : quotes) {
-            String[] words = quote.split("\\w+");
+            String[] words = quote.toLowerCase().split("\\W+");
             Set<String> set = new HashSet<>();
             for (String word : words) {
+                if (!map.containsKey(word)) {
+                    continue;
+                }
                 int[] curNums = map.get(word);
                 if (!set.contains(word)) {
                     curNums[0]++;
@@ -50,7 +53,7 @@ public class TopNBuzzwords {
                     return b.getKey().compareTo(a.getKey());
                 }
             } else {
-                return a.getValue()[0] - b.getValue()[0]; 
+                return a.getValue()[1] - b.getValue()[1]; 
             }
         });
 
@@ -62,97 +65,31 @@ public class TopNBuzzwords {
         }
 
         while (!queue.isEmpty()) {
-            result.add(0, queue.poll().getKey());
+            Map.Entry<String, int[]> curEntry = queue.poll();
+            if (curEntry.getValue()[1] > 0) {
+                result.add(0, curEntry.getKey());
+            }    
         }
 
         return result;
     }
 
     public static void main(String[] args) {
-        String quote = "Elmo is the hottest of the season! Elmo will be on every kid's wishlist!";
-        String[] words = quote.toLowerCase().split("\\W+");
-        for (String word : words) {
-            System.out.println(word);
-        }
-    }
-}
+        int topToys = 5;
+        int numToys = 0;
+        // String[] toys = {"elmo", "elsa", "legos", "drone", "tablet", "warcraft"};
+        String[] toys = {};
+        int numQuotes = 6;
+        String[] quotes = {
+            "Elmo is the hottest of the season! Elmo will be on every kid's wishlist!",
+            "The new Elmo dolls are super high quality",
+            "Expect the Elsa dolls to be very popular this year, Elsa!",
+            "Elsa and Elmo are the toys I'll be buying for my kids, Elsa is good",
+            "Elsa For parents of older kids, look into buying them a drone",
+            "Warcraft is slowly rising in popularity ahead of the holiday season" 
+        };
 
-
-
-
-// "static void main" must be defined in a public class.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(topToys(6, 2, new String[] { "elmo", "elsa", "legos", "drone", "tablet", "warcraft" }, 6,
-                new String[] { "Elmo is the hottest of the season! Elmo will be on every kid's wishlist!",
-                        "The new Elmo dolls are super high quality",
-                        "Expect the Elsa dolls to be very popular this year, Elsa!",
-                        "Elsa and Elmo are the toys I'll be buying for my kids, Elsa is good",
-                        "For parents of older kids, look into buying them a drone",
-                        "Warcraft is slowly rising in popularity ahead of the holiday season" }));
-    }
-
-    public static List<String> topToys(int numToys, int topToys, String[] toys, int numQuotes, String[] quotes) {
-        Map<String, int[]> freq = new HashMap<>();
-        for (String toy : toys) {
-            freq.put(toy, new int[] { 0, 0 });
-        }
-
-        for (String quote : quotes) {
-            Set<String> used = new HashSet<>();
-
-            String[] words = quote.toLowerCase().split("\\W+");
-            for (String word : words) {
-                if (!freq.containsKey(word)) {
-                    continue;
-                }
-
-                int[] nums = freq.get(word);
-
-                nums[0]++;
-                if (!used.contains(word)) {
-                    nums[1]++;
-                }
-
-                used.add(word);
-            }
-        }
-
-        PriorityQueue<String> pq = new PriorityQueue<>((t1, t2) -> {
-            if (freq.get(t1)[0] != freq.get(t2)[0]) {
-                return freq.get(t1)[0] - freq.get(t2)[0];
-            }
-
-            if (freq.get(t1)[1] != freq.get(t2)[1]) {
-                return freq.get(t1)[1] - freq.get(t2)[1];
-            }
-
-            return t2.compareTo(t1);
-        });
-
-        if (topToys > numToys) {
-            for (String toy : freq.keySet()) {
-                if (freq.get(toy)[0] > 0) {
-                    pq.add(toy);
-                }
-            }
-        } else {
-            for (String toy : toys) {
-                pq.add(toy);
-
-                if (pq.size() > topToys) {
-                    pq.poll();
-                }
-            }
-        }
-
-        List<String> output = new ArrayList<>();
-        while (!pq.isEmpty()) {
-            output.add(pq.poll());
-        }
-
-        Collections.reverse(output);
-
-        return output;
+        List<String> result = topNBuzzwords(numToys, topToys, toys, numQuotes, quotes);
+        System.out.println(result);
     }
 }
