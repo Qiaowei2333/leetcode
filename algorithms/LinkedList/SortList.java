0a15c3a46fa437f3adeb0becb7f8b83a0effa424
Qiaowei Li <qiaoweili@Qiaoweis-MBP.attlocal.net>
2020-08-12 22:20:20 -0500

    lc148 sort list update findmid method

--- algorithms/LinkedList/SortList.java
+++ algorithms/LinkedList/SortList.java
@@ -15,13 +15,8 @@ public class SortList {
     private ListNode findMid(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while (fast.next != null) {
-            if (fast.next != null && fast.next.next != null) {
-                fast = fast.next.next;
-            }
-            else {
-                break;
-            }
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
             slow = slow.next;
         }
         return slow;
