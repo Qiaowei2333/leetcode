ad6ff0c9937cd576242703e8d559ee78c67d2ed3
Qiaowei Li <qiaoweili@Qiaoweis-MBP.attlocal.net>
2020-09-01 08:46:35 -0500

    update binary search

--- algorithms/BinarySearch/BinarySearch.java
+++ algorithms/BinarySearch/BinarySearch.java
@@ -1,6 +1,25 @@
 import java.util.*;
-
+// lc 704
 public class BinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] > target) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
     public int binarySearch(int[] nums, int target) {
         if(nums == null || nums.length == 0) {
             return -1;
