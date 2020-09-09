f90ae898ed6afd70b1ba28a89e3800613e9ec575
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2020-09-09 16:35:28 -0500

    lc283: update sol

--- algorithms/ebay/MoveZeroes.java
+++ algorithms/ebay/MoveZeroes.java
@@ -1,16 +1,16 @@
 // lc 283
 public class MoveZeroes {
     public void moveZeroes(int[] nums) {
-        int left = 0;
-         for (int right = 0; right < nums.length; right++) {
-             if (nums[right] != 0) {
-                 nums[left] = nums[right];
-                 left++;
-             }
-         }
-         
-         for (int i = left; i < nums.length; i++) {
-             nums[i] = 0;
-         }
-     }
+        if (nums == null || nums.length == 0) return;
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow++] = nums[fast];
+            }
+            fast++;
+        }
+        while (slow < nums.length) {
+            nums[slow++] = 0;
+        }
+    }
 }
