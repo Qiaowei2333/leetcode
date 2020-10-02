ec23042ab587ae025f0c4b0b417447ea9e883c2c
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2020-10-01 21:35:22 -0500

    two pointers quick sort

---
+++ algorithms/SortArray/SortColor2.java
@@ -0,0 +1,45 @@
+// lint 143 quick sort sol1, counting sort sol2
+public class SortColor2 {
+    public void sortColors2(int[] colors, int k) {
+        quickSort(colors, 0, colors.length - 1);
+    }
+
+    private void quickSort(int[] nums, int start, int end) {
+        if (start <= end) {
+            int pivot = partition(nums, start, end);
+            quickSort(nums, start, pivot - 1);
+            quickSort(nums, pivot + 1, end);
+        }
+    }
+
+    private int partition(int[] nums, int start, int end) {
+        int pivotValue = nums[end];
+        int pivotIndex = start;
+        for (int i = start; i < end; i++) {
+            if (nums[i] < pivotValue) {
+                swap(nums, i, pivotIndex);
+                pivotIndex++;
+            }
+        }
+        swap(nums, pivotIndex, end);
+        return pivotIndex; 
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        if (i == j) return;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        SortColor2 s = new SortColor2();
+        int[] input = new int[]{3,2,2,1,4};
+        s.sortColors2(input, 4);
+        for (int elm : input) {
+            System.out.println(elm);
+        }
+        // int privotIndex = s.partition(new int[]{7, 6, 1, 2, 3}, 0, 4);
+        // System.out.println(privotIndex);
+    }
+}
