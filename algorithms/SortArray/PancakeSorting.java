e68616adb04dc54a5c6e97c06ba5bba890b89132
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2020-10-01 23:35:50 -0500

    add comment

--- algorithms/SortArray/PancakeSorting.java
+++ algorithms/SortArray/PancakeSorting.java
@@ -2,10 +2,11 @@
 // https://www.geeksforgeeks.org/pancake-sorting/
 import java.util.*;
 public class PancakeSorting {
+    // time O(n^2) space O(n)   
     public List<Integer> pancakeSort(int[] arr) {
         List<Integer> res = new ArrayList<>();
         for (int i = arr.length; i > 1; i--) {
-            int maxIndex = findMax(arr, 0, i - 1);
+            int maxIndex = findMax(arr, 0, i - 1); // for (i = 0; A[i] != x; ++i);  find max because max = curLength, 根据题目，nums.length个数，都不同，且在 1 <= nums[i] <= nums.length，每次找的max就是length
             flip(arr, maxIndex + 1);
             res.add(maxIndex + 1);
             flip(arr, i);
