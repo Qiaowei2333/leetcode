d23169c73b0bb49314d8b76d5630cc67dd92eca3
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2020-10-17 20:18:48 -0500

    update sol

--- algorithms/ebay/FindMedianFromDataStream.java
+++ algorithms/ebay/FindMedianFromDataStream.java
@@ -1,65 +1,37 @@
+// lc 295
 import java.util.Queue;
 
 public class FindMedianFromDataStream {
     /** initialize your data structure here. */
-    Queue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b.compareTo(a));
+    Queue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.valueOf(b) - Integer.valueOf(a));
 	   
     Queue<Integer> minHeap = new PriorityQueue<>();
      // Adds a number into the data structure.
     public void addNum(int num) {
-        if (minHeap.isEmpty() && maxHeap.isEmpty()) {
+        if (maxHeap.isEmpty()) {
             maxHeap.offer(num);
             return;
         }
-        
-        if (num == 3) {
-            System.out.println("start");
-        }
-        if (minHeap.isEmpty()) {
-            int topMax = maxHeap.poll();
-            if (num > topMax) {
-                minHeap.offer(num);
-                maxHeap.offer(topMax);
-            } 
-            else {
-                maxHeap.offer(num);
-                minHeap.offer(topMax);
-            }
-            return;
-        }
 
-        if (num >= maxHeap.peek() && num <= minHeap.peek()) {
-            if (maxHeap.size() == minHeap.size()) {
+        if (maxHeap.size() == minHeap.size()) {
+            if (num <= minHeap.peek()) {
                 maxHeap.offer(num);
             }
             else {
-                minHeap.offer(num);
-            }
-            return;
-        }
-
-        if (num <= maxHeap.peek()) {
-            if (minHeap.size() == maxHeap.size()) {
-                maxHeap.offer(num);
-            }
-            else {
-                maxHeap.offer(num);
-                minHeap.offer(maxHeap.poll());
-            }
-            return;
-        }
-
-        if (num >= minHeap.peek()) {
-            
-            if (minHeap.size() == maxHeap.size()) {
-                minHeap.offer(num);
                 maxHeap.offer(minHeap.poll());
-                
-            }
-            else {
                 minHeap.offer(num);
             }
-            return;
+        }
+        else {
+            if (maxHeap.size() > minHeap.size()) {
+                if (num >= maxHeap.peek()) {
+                    minHeap.offer(num);
+                }
+                else {
+                    minHeap.offer(maxHeap.poll());
+                    maxHeap.offer(num);
+                }
+            }
         }
     }
  
