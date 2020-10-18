af7d53a4cab693a162bd77829fe98135b54e1048
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2020-10-17 22:40:28 -0500

    lc378 10/17

---
+++ algorithms/Queue/KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,33 @@
+// lc 378 类似 MergeKSortedArray
+import java.util.*;
+public class KthSmallestElementInASortedMatrix {
+    public int kthSmallest(int[][] matrix, int k) {
+        Queue<Tuple> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        for (int i = 0; i < matrix.length; i++) {
+            int val = matrix[i][0];
+            pq.offer(new Tuple(i, 0, val));
+        }
+
+        for (int j = k; j > 1; j--) {
+            Tuple cur = pq.poll();
+            int x = cur.x;
+            int y = cur.y + 1;
+            if (y < matrix.length) {
+                int val = matrix[x][y];
+                pq.offer(new Tuple(x, y, val));
+            }
+        }
+        return pq.peek().val;
+    }
+    
+    static class Tuple {
+        int x;
+        int y;
+        int val;
+        public Tuple(int x, int y, int val) {
+            this.x = x;
+            this.y = y;
+            this.val = val;
+        }
+    }
+}
