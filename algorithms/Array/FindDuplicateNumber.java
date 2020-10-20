89825eea253476603c7ee7d2a5347a32dc24ec0c
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2020-10-19 21:58:52 -0500

    binary search sol

--- algorithms/Array/FindDuplicateNumber.java
+++ algorithms/Array/FindDuplicateNumber.java
@@ -18,4 +18,18 @@ public class FindDuplicateNumber {
             }
         }
     }
+    // binary search sol  time O(nlogn) space O(1) https://leetcode.com/problems/find-the-duplicate-number/discuss/72841/Java-O(1)space-using-Binary-Search
+    public int findDuplicate2(int[] nums) {
+        int low = 1, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (int) (low + (high - low) * 0.5);
+            int cnt = 0;
+            for (int a : nums) {
+                if (a <= mid) ++cnt;
+            }
+            if (cnt <= mid) low = mid + 1;
+            else high = mid - 1;
+        }
+        return low;
+    }
 }
