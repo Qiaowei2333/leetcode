666b172c3f67864c8b30742eafbaa4ae1cfa1e04
Qiaowei Li <qiaoweili@Qiaoweis-MBP.attlocal.net>
2020-11-07 15:30:39 -0600

    update sol bfs dfs

--- algorithms/yama/NumberOfIslands.java
+++ algorithms/yama/NumberOfIslands.java
@@ -2,57 +2,64 @@
 // space O(min(row, col))
 import java.util.*;
 public class NumberOfIslands {
-    static class Coordinate {
-        int row;
-        int col;
-        public Coordinate(int x, int y) {
-            this.row = x;
-            this.col = y;
-        } 
-    }
-
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
-        int numOfIslands = 0;
+        int count = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
-                    checkAndMark(i, j, grid);
-                    numOfIslands++;
+                    count++;
+                    // bfs(grid, i, j);
+                    dfs(grid, i, j);
                 }
             }
         }
-        return numOfIslands;
+        return count;
     }
-
-    private void checkAndMark(int row, int col, char[][]grid) {
-        Queue<Coordinate> queue = new LinkedList<>();
-        queue.offer(new Coordinate(row, col));
-        grid[row][col] = '0';
+    
+    private void dfs(char[][] grid, int i, int j) {
+        if (!isValid(i, j, grid)) return;
+        grid[i][j] = '0';
+        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        for (int[] dir : dirs) {
+            int r = dir[0] + i;
+            int c = dir[1] + j;
+            dfs(grid, r, c);
+        }
+    }
+    
+    private void bfs(char[][] grid, int i, int j) {
+        Queue<Pos> queue = new LinkedList<>();
+        grid[i][j] = '0';
+        queue.offer(new Pos(i, j));
         int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         while (!queue.isEmpty()) {
-            Coordinate curCoor = queue.poll();
-            for (int i = 0; i < dirs.length; i++) {
-                int[] dir = dirs[i];
-                int newRow = curCoor.row + dir[0];
-                int newCol = curCoor.col + dir[1];
-                if (isLand(newRow, newCol, grid)) {
-                    grid[newRow][newCol] = '0';
-                    queue.offer(new Coordinate(newRow, newCol));
+            Pos curPos = queue.poll();
+            for (int[] dir : dirs) {
+                int r = dir[0] + curPos.row;
+                int c = dir[1] + curPos.col;
+                if (isValid(r, c, grid)) {
+                    grid[r][c] = '0';
+                    queue.offer(new Pos(r, c));
                 }
             }
         }
     }
-
-    private boolean isLand(int row, int col, char[][] grid) {
-        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+    
+    private boolean isValid(int r, int c, char[][] grid) {
+        if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == '1') {
+           return true; 
+        }
+        else {
             return false;
-        } else if (grid[row][col] == '0') {
-            return false;
-        } else {
-            return true;
+        }
+    }
+    
+    static class Pos {
+        int row;
+        int col;
+        public Pos(int x, int y) {
+            row = x;
+            col = y;
         }
     }
     public static void main(String[] args) {
