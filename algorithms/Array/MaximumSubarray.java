88123f2036b7a660ad6884a3b93ede5e4e754aac
Qiaowei Li <qiaoweili@Qiaoweis-MBP.attlocal.net>
2020-11-14 17:38:48 -0600

    update sol dp  add comment

--- algorithms/Array/MaximumSubarray.java
+++ algorithms/Array/MaximumSubarray.java
@@ -1,9 +1,9 @@
 // lc 53 
 public class MaximumSubarray {
     // dp   time O(n)  space O(n)
-    public int maxSubArray(int[] nums) {
+    public static int maxSubArray(int[] nums) {
         if (nums.length < 2) return nums[0];
-        int[] d = new int[nums.length];
+        int[] d = new int[nums.length];// d[i] 到i位置，包括i元素的最大的sum  
         d[0] = nums[0];
         int max = d[0];
         for (int i = 1; i < d.length; i++) {
@@ -11,6 +11,12 @@ public class MaximumSubarray {
             max = Math.max(max, d[i]);
         }
         return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int max = maxSubArray(nums);
+        System.out.println(max);
     }
 
     // 
