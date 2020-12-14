1b520451790584f3b17ab00af35d31c69255f98a
Qiaowei Li <qiaoweili@Qiaoweis-MBP.attlocal.net>
2020-12-14 07:45:46 -0600

    update method

---
+++ algorithms/BinarySearch/FindMinInRotatedSortedArray.java
@@ -0,0 +1,25 @@
+public class FindMinInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int target = nums[start];
+        // find first element < nums[start]
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                start = mid + 1;
+            }
+            else if (nums[mid] > target) {
+                start = mid + 1;
+            }
+            else if (nums[mid] < target) {
+                end = mid - 1;
+            }
+        }
+
+        if (start < nums.length) {
+            return nums[start];
+        }
+        return nums[0];
+    }
+}
