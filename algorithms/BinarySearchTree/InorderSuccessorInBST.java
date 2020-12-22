ee823be5f2c579ac29591fa5e53068b9dec19250
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2020-12-22 09:34:19 -0600

    lc 285

--- algorithms/BinarySearchTree/InorderSuccessorInBST.java
+++ algorithms/BinarySearchTree/InorderSuccessorInBST.java
@@ -1,3 +1,4 @@
+// lc 285
 public class InorderSuccessorInBST {
     // SOL 1 inorder traversal O(n)
     private TreeNode prev;
@@ -19,4 +20,22 @@ public class InorderSuccessorInBST {
     }
 
     // sol 2 O(h) h is logN, height of the tree
+    //To find a successor, you just need to find the smallest one that is larger than the given value since there are no duplicate values in a BST.
+    //https://leetcode.com/problems/inorder-successor-in-bst/discuss/72656/JavaPython-solution-O(h)-time-and-O(1)-space-iterative
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode succ = null;
+        while (root != null) {
+            if (root.val > p.val) {
+                succ = root;
+                root = root.left;
+            }
+            else if (root.val < p.val) {
+                root = root.right;
+            } 
+            else if (root.val == p.val) {
+                root = root.right;
+            }
+        }
+        return succ;
+    }
 }
