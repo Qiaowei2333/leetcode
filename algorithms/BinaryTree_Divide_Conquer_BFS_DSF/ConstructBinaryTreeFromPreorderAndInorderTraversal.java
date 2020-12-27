fca1057f1e84d1b9a4cd5e415e1cae9c614db2a7
Qiaowei2333 <qxl161930@utdallas.edu>
2020-12-27 07:48:40 -0600

    lc 105

---
+++ algorithms/BinaryTree_Divide_Conquer_BFS_DSF/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,22 @@
+// lc 105
+import java.util.*;
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inmap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            inmap.put(inorder[i], i);
+        }
+        return buildTree(preorder, inorder, 0, 0, inorder.length - 1, inmap);
+    }
+    
+    private TreeNode buildTree(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd, Map<Integer, Integer> inmap) {
+        if (inStart > inEnd) return null;
+        int val = preorder[preStart];
+        TreeNode cur = new TreeNode(val);
+        int inRoot = inmap.get(val);
+        int numLeft = inRoot - inStart + 1;
+        cur.left = buildTree(preorder, inorder, preStart + 1, inStart, inRoot - 1, inmap);
+        cur.right = buildTree(preorder, inorder, preStart + numLeft, inRoot + 1, inEnd, inmap);
+        return cur;
+    }
+}
