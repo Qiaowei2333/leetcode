9ce74526d711cc60a7c7053835119e13967d273b
Qiaowei2333 <qxl161930@utdallas.edu>
2021-01-24 15:15:51 -0600

    lc 26

--- algorithms/SlidingWindow(TwoPointers)/RemoveDuplicatesFromSortedArray.java
+++ algorithms/SlidingWindow(TwoPointers)/RemoveDuplicatesFromSortedArray.java
@@ -3,14 +3,13 @@
 public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
         if (nums == null || nums.length == 0) return 0;
-        if (nums.length == 1) return 1;
-        int slow = 1, fast = 1;
-        while (fast < nums.length) {
-            if (nums[fast] != nums[fast - 1]) {
-                nums[slow++] = nums[fast];
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            if (nums[r] > nums[l]) {
+                l++;
+                nums[l] = nums[r];
             }
-            fast++;
         }
-        return slow;
+        return l + 1;
     }
 }
