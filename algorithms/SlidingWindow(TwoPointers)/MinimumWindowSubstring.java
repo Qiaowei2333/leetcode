dcbe1eda9059e99f5d945d2960303268fd9dd8cd
Qiaowei2333 <qxl161930@utdallas.edu>
2021-01-24 09:33:59 -0600

    lc 76

--- algorithms/SlidingWindow(TwoPointers)/MinimumWindowSubstring.java
+++ algorithms/SlidingWindow(TwoPointers)/MinimumWindowSubstring.java
@@ -2,41 +2,27 @@
 // https://www.youtube.com/watch?v=63i802XLgOM&ab_channel=basketwangCoding
 public class MinimumWindowSubstring {
     public String minWindow(String s, String t) {
-        int[] tMap = new int[256];
-        int[] sMap = new int[256];
-        
-        for (char ch : t.toCharArray()) {
-            tMap[ch]++;
+        int[] tMap = new int[128];
+        int[] sMap = new int[128];
+        for (char c : t.toCharArray()) {
+            tMap[c]++;
         }
-        
-        int count = 0;
-        int tSize = t.length();
         int l = 0;
+        int count = 0;
         String res = "";
         for (int r = 0; r < s.length(); r++) {
             char cur = s.charAt(r);
             if (tMap[cur] == 0) continue;
             sMap[cur]++;
-            if (sMap[cur] <= tMap[cur]) {
-                if (count == 0) l = r;
-                count++;
-            }
-            while (count == tSize) {
-                if (res.equals("")) {
-                    res = s.substring(l, r + 1);
-                   
-                }
-                else {
-                    if (res.length() > r - l + 1) {
+            if (count == 0) l = r;
+            if (sMap[cur] <= tMap[cur]) count++;
+            while (count == t.length()) {
+                char lch = s.charAt(l);
+                if (tMap[lch] != 0) {
+                    if (res.isEmpty() || res.length() > r + 1 - l)
                         res = s.substring(l, r + 1);
-                    }
-                }
-                char lchar = s.charAt(l);
-                if (tMap[lchar] != 0) {
-                    sMap[lchar]--;
-                    if (sMap[lchar] < tMap[lchar]) {
-                        count--;
-                    } 
+                    sMap[lch]--;
+                    if (sMap[lch] < tMap[lch]) count--;
                 }
                 l++;
             }
