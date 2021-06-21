fb7429e4d7bad8adfd02384d9a2abf3ce04f983d
Qiaowei2333 <qxl161930@utdallas.edu>
2021-06-21 00:14:07 -0500

    sol added

--- algorithms/DP/MaximumSubarray.java
+++ algorithms/DP/MaximumSubarray.java
@@ -13,6 +13,28 @@ public class MaximumSubarray {
         return max;
     }
 
+    // greedy思想   time O(n)  space O(1)
+    // https://www.geeksforgeeks.org/maximum-difference-between-two-elements/
+    // method2
+    public int maxSubArrayGreedy(int[] nums) {
+        if (nums.length < 2) return nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        
+        int lowestValue = 0, r = 0;
+        int maxDiff = nums[0];
+        for (;r < nums.length; r++) {
+            if (nums[r] - lowestValue > maxDiff) {
+                maxDiff = nums[r] - lowestValue;
+            }
+            if (nums[r] < lowestValue) {
+                lowestValue = nums[r];
+            }
+        }
+        return maxDiff;
+    }
+
     public static void main(String[] args) {
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
         int max = maxSubArray(nums);
