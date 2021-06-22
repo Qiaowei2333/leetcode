6a5b49b75739b7fe9dfd871fac3fad74926521c6
Qiaowei2333 <qxl161930@utdallas.edu>
2021-06-21 22:00:52 -0500

    update sol

--- algorithms/DP/MinCostClimbingStairs.java
+++ algorithms/DP/MinCostClimbingStairs.java
@@ -2,14 +2,13 @@
 public class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        int[] dp = new int[n + 1]; // dp[i] - minCost at i, 最右加了一位作为结果dp[n]
-        for (int i = 0; i < n; i++) {
-            dp[i] = cost[i];
+        int[] dp = new int[n]; //dp[i] - min cost at index i, which you already pay cost[i], and are able to move to i + 1, i + 2
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
         }
         
-        for (int j = 2; j < dp.length; j++) {
-            dp[j] = Math.min(dp[j - 1], dp[j - 2]) + dp[j];
-        }
-        return dp[n];
+        return Math.min(dp[n - 1], dp[n - 2]);
     }
 }
