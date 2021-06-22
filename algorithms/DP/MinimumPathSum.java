594c7aed91c70547b09b92ac6fd366d6e2fa41c4
Qiaowei2333 <qxl161930@utdallas.edu>
2021-06-22 00:40:25 -0500

    update sol

--- algorithms/DP/MinimumPathSum.java
+++ algorithms/DP/MinimumPathSum.java
@@ -2,19 +2,24 @@
 public class MinimumPathSum {
     // dp[i][j] 含义： 起点(1, 1)到(i, j)位置的minimum path sum
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (i == 1 || j == 1) {
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]) + grid[i - 1][j - 1];
-                }
-                else {
-                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i - 1][j - 1];  
-                }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = grid[0][i] + dp[0][i - 1];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
             }
         }
-        return dp[n][m];
+        
+        return dp[m - 1][n - 1];
     }
 }
