272a0b5c3a0d4dcee4976e493549b13d041d18b5
Qiaowei2333 <qxl161930@utdallas.edu>
2021-07-07 22:15:54 -0500

    update

--- algorithms/DP/CoinChange.java
+++ algorithms/DP/CoinChange.java
@@ -49,28 +49,9 @@ public class CoinChange {
         return dp[amount] == amount + 1 ? -1 : dp[amount];
     }
 
-    public int coinChangeDPP(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, amount + 1);
-        Set<Integer> set = new HashSet<>();
-        for (int coin : coins) {
-            set.add(coin);    
-        }
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-           for (int j = 0; j < i; j++) {
-               int diff = i - j;
-               if (set.contains(diff)) {
-                   dp[i] = Math.min(dp[i], dp[j] + 1);
-               }
-           } 
-        }
-        return dp[amount] == amount + 1 ? -1 : dp[amount];
-    }
-
     public static void main(String[] args) {
         CoinChange s = new CoinChange();
-        int res = s.coinChangeDPP(new int[]{484,395,346,103,329}, 4259);
+        int res = s.coinChange(new int[]{484,395,346,103,329}, 4259);
         System.out.println(res);
     }
 }
