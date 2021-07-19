2886bc267c4b55f1ff5eef2c890d265e3381ba14
Qiaowei2333 <qxl161930@utdallas.edu>
2021-07-18 23:56:29 -0500

    update sol

--- algorithms/BinarySearch/SearchInRotatedSortedArray.java
+++ algorithms/BinarySearch/SearchInRotatedSortedArray.java
@@ -7,33 +7,6 @@ public class SearchInRotatedSortedArray {
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (target == nums[mid]) return mid;
-            if (nums[mid] >= nums[start]) {
-                if (target >= nums[start] && target < nums[mid]) {
-                    end = mid - 1;
-                }
-                else {
-                    start = mid + 1;
-                }
-            }
-            else {
-                if (target <= nums[end] && target > nums[mid]) {
-                    start = mid + 1;
-                }
-                else {
-                    end = mid - 1;
-                }
-            }
-        }
-        return -1;
-    }
-
-    public int search1(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
-        int start = 0; 
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (target == nums[mid]) return mid;
             if (target >= nums[0]) {
                 if (nums[mid] > target || nums[mid] < nums[0]) {
                     end = mid - 1;
