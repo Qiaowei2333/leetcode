7b75c905c36c211ed43eed72f5f0a70fe53c30b4
Qiaowei2333 <qxl161930@utdallas.edu>
2021-08-15 18:58:26 -0500

    update sol

--- algorithms/bb/RemoveAllAdjacentDuplicatesInString2.java
+++ algorithms/bb/RemoveAllAdjacentDuplicatesInString2.java
@@ -1,49 +1,40 @@
 // lc 1209
 import java.util.*;
 public class RemoveAllAdjacentDuplicatesInString2 {
-    private class Node {
-        char c;
-        int count;
-        public Node(char c, int count) {
-            this.c = c;
-            this.count = count;
-        }
-    }
-    
     public String removeDuplicates(String s, int k) {
-        if (s.length() == 1) {
-            return s;
-        }
         Stack<Node> stack = new Stack<>();
-        
         char[] charArr = s.toCharArray();
-        stack.push(new Node(charArr[0], 1));
-        for (int i = 1; i < charArr.length; i++) {
-            if (stack.isEmpty()) {
+
+        for (int i = 0; i < charArr.length; i++) {
+            if (stack.isEmpty() || stack.peek().c != charArr[i]) {
                 stack.push(new Node(charArr[i], 1));
-                continue;
             }
-            Node preNode = stack.peek();
-            if (charArr[i] == preNode.c) {
-                if (preNode.count == k - 1) {
+            else {
+                Node cur = stack.peek();
+                cur.count++;
+                
+                if (cur.count == k) {
                     stack.pop();
                 }
-                else {
-                    preNode.count++;
-                }
-            } 
-            else {
-                stack.push(new Node(charArr[i], 1));
             }
         }
         
         StringBuilder sb = new StringBuilder();
-        for (Node n : stack) {
+        for (Node n : stack) { // use enhanced for loop to iterate a stack in FIFO way
             for (int i = 0; i < n.count; i++) {
                 sb.append(n.c);
             }
         }
         
         return sb.toString();
+    }
+}
+
+class Node {
+    char c;
+    int count;
+    public Node(char c, int count) {
+        this.c = c;
+        this.count = count;
     }
 }
