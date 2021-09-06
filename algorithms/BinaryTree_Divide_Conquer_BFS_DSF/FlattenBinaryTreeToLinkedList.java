4ede4f8f7fc96086df8c43b70ded0da73adf8769
Qiaowei2333 <qxl161930@utdallas.edu>
2021-09-06 01:48:45 -0500

    update sol

--- algorithms/BinaryTree_Divide_Conquer_BFS_DSF/FlattenBinaryTreeToLinkedList.java
+++ algorithms/BinaryTree_Divide_Conquer_BFS_DSF/FlattenBinaryTreeToLinkedList.java
@@ -1,29 +1,41 @@
+// lc 114
+import java.util.*;
 public class FlattenBinaryTreeToLinkedList {
+    // Best solution - time O(n), space O(1) 
+    // 右左根遍历，利用prev，反向联起来，就是根左右的顺序
+    private TreeNode pre = null;
+    public void flatten1(TreeNode root) {
+        if (root == null) return;
+        flatten1(root.right);
+        flatten1(root.left);
+        // System.out.println(root.val);
+        root.right = pre;
+        root.left = null;
+        pre = root;
+    }
+
+    // 上一个解法是利用右左根的遍历，这个是右左根的stack的解法
+    // 右左根实际上就是根左右反过来 result.add(0, cur.val)
+    // 所以这里实际上写的是preorder遍历
     public void flatten(TreeNode root) {
-        postOrder(root);
-        return;
-    }
-    // note: return leftmost rightmost node, not min max value, this is not bst!!
-    private PairMinMax postOrder(TreeNode root) {
-        if (root == null) return null;
-        PairMinMax leftSub = postOrder(root.left);
-        PairMinMax rightSub = postOrder(root.right);
-        PairMinMax cur = new PairMinMax();
-        cur.min = root;
-        cur.max = root;
-        if (leftSub != null) {
-            root.left = null;
-            root.right = leftSub.min;
-            cur.max = leftSub.max;
+        if (root == null) return;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            
+            // result.add(0, cur.val); 此时 result是右左根
+            if (cur.right != null) stack.push(cur.right);
+            if (cur.left != null ) stack.push(cur.left);
+            
+            cur.left = null;
+            // System.out.println(cur.val);
+            if (!stack.isEmpty()) cur.right = stack.peek();
         }
-        if (rightSub != null) {
-            if (leftSub != null) {
-                leftSub.max.right = rightSub.min;
-            }
-            cur.max = rightSub.max;
-        }
-        return cur;
     }
+
+
     
     static class PairMinMax {
         TreeNode min;
