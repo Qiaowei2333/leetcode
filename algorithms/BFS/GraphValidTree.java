879956b1152f406f27fd4901f86fa245304178c0
Qiaowei2333 <qxl161930@utdallas.edu>
2021-09-14 01:17:20 -0500

    add comment

--- algorithms/BFS/GraphValidTree.java
+++ algorithms/BFS/GraphValidTree.java
@@ -1,16 +1,18 @@
 // lc 261
+// 注意每个点都是不一样的值，从0到n-1
 import java.util.*;
 class GraphValidTree {
     public boolean validTree(int n, int[][] edges) {
         if (n == 0) {
             return false;
         }
-        // 节点数 - 1 = 边的数，用来判断有没有环
+        // tree 必须满足 节点数 - 1 = 边的数
         if (n - 1 != edges.length) {
             return false;
         }
 
-        // List<List<Integer>> 也行
+        // List<List<Integer>> 也行，因为每个点都是不一样的，从0到n-1
+        // Map<Integer, List<Integer>> 也许
         Map<Integer, Set<Integer>>graph = iniUndirectedGraph(n, edges);
         
         Queue<Integer> queue = new LinkedList<>();
@@ -18,7 +20,7 @@ class GraphValidTree {
         
         queue.add(0);
         set.add(0);
-        // bfs 看是否联通
+        // 在树的边数已经确定 = n - 1的条件下， bfs 来看是否联通
         while (!queue.isEmpty()) {
             int curV = queue.poll();
             for (Integer neighbor : graph.get(curV)) {
