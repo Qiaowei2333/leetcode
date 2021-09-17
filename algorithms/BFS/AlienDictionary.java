61c6efcd3fb54f433894a8cee9aa751274faccd4
Qiaowei Li <Qiaowei.Li@tdindustries.com>
2021-09-16 23:34:26 -0500

    add comment

--- algorithms/BFS/AlienDictionary.java
+++ algorithms/BFS/AlienDictionary.java
@@ -3,9 +3,12 @@ import java.util.*;
 class AlienDictionary {
     // time O(n) n - total number of characters in words
     // space O(1) 我觉得趋近于常数 因为最多26个character
+    // 注意有环的cornor case ["z","x","a","zb","zx"]， ["z","x","z"]， ["zxa","zx"]
+    // 为什么最后要比较res.length和map.size()是否相等 line 30，注意第一个cornor case，本来是返回空string的，但是如果不比较，会返回"b"
+    // 注意第三个conor case，根本构不成图，所以在构图是要检查最后的长度 line 58
     public String alienOrder(String[] words) {
         Map<Character, Integer> indegree = new HashMap<>();
-        Map<Character, Set<Character>> graph = new HashMap<>();
+        Map<Character, Set<Character>> graph = new HashMap<>(); // 为什么必须是set，不能是list，因为为了计算入度 line 53，必须先检查是否之前已经出现过了
         if (!buildGraph(graph, indegree, words)) {
             return "";
         }
@@ -46,7 +49,8 @@ class AlienDictionary {
             int minLen = Math.min(a.length(), b.length());
             while (i < minLen) {
                 if (a.charAt(i) != b.charAt(i)) {
-                    if (!graph.get(a.charAt(i)).contains(b.charAt(i))) indegree.put(b.charAt(i), indegree.getOrDefault(b.charAt(i), 0) + 1);
+                    if (!graph.get(a.charAt(i)).contains(b.charAt(i))) 
+                        indegree.put(b.charAt(i), indegree.getOrDefault(b.charAt(i), 0) + 1);
                     graph.get(a.charAt(i)).add(b.charAt(i));
                     break;
                 }
