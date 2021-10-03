710d8516a22b368c790353c282fe2a32d5ecaa9d
Qiaowei2333 <qxl161930@utdallas.edu>
2021-10-03 01:05:58 -0500

    lc 212

--- algorithms/DFS/WordSearch2.java
+++ algorithms/DFS/WordSearch2.java
@@ -1,20 +1,25 @@
-// lc 79
-public class WordSearch {
-    // time O(n * m * 3^l) n - col, m - row, l - word length
-    // 想像成三叉树，每次都要走到depth = l的地步，就如同一个完全的三叉树
-    // 节点数应该是3^l个， 每个点都走dfs，都会形成这样的树，一共m*n个点
-    // 所以一共是m*n*3^l
-    // worst case:
-    // a a a a a a a ... a
-    // a a a a a a a ... a
-    // . . . . . . . . . a
-    // . . . . . . . . . a
-    // a a a a a a a ..a b
-    //
-    // word = aaab
+// lc 212, 类似 lc 79，但是可以用trie来解
+import java.util.*;
+public class WordSearch2 {
+    // sol usng trie
+    // https://leetcode.com/problems/word-search-ii/discuss/59780/Java-15ms-Easiest-Solution-(100.00)
+
+    // sol using lc 79 - time O(len * n * m * 3^l) n - col, m - row, l - word avg length, len - words length
     private boolean founded = false;
     private int[][] dirs = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    public boolean exist(char[][] board, String word) {
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        for (String word : words) {
+            if (exist(board, word)) {
+                res.add(word);
+                this.founded = false;
+            }
+        }
+        return res;
+    }
+    
+    private boolean exist(char[][] board, String word) {
         boolean[][] visited = new boolean[board.length][board[0].length];
 
         for (int i = 0; i < board.length; i++) {
