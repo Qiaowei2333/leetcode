f82bd3f4425b6de03c4f3d44d18ff87a157d72ac
Qiaowei2333 <qxl161930@utdallas.edu>
2021-11-12 00:19:30 -0600

    update

--- algorithms/Array/FindDuplicateNumber.java
+++ algorithms/Array/FindDuplicateNumber.java
@@ -1,4 +1,4 @@
-// lc287 smiliar to linkedlist cycle 2 lc142 
+// lc 287 smiliar to linkedlist cycle 2 lc142 
 // 注意条件 each integer 都在[1, n]范围内，这样可以保证第一个nums[0]肯定不是环，如果范围改成[0, n], 要用xor做
 // 假如范围是[0, n], 比如[0, 1, 1], 0本身就是一个环，程序永远找不到这个1
 public class FindDuplicateNumber {
